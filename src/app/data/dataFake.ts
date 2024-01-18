5aab957639ca2e36b224d0edb3ab410c63ec709c
Emanuel Albuqueruque <emanuel.albuquerque.1995@gmail.com>
2024-01-18 16:34:19 -0300

    Deixando a Content Page dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,27 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"aaa",
-    "title": "saiu novo filme",
-    "description":"ola"
+    "id":"1",
+    "title": "WOLVERINE LOGAN",
+    "description":"Em entrevista à Vanity Fair (via Entertainment Weekly), o astro enfrentou um detector de mentiras, e foi questionado se sua forma física atual era uma preparação para viver o herói. Radcliffe respondeu com um “não.” O ator britânico ainda comenta que a fase “maromba”, que viralizou com a quarta temporada de Miracle Workers, vem por influência dos pais.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTi_bGStEt9vfDXsGjulKZoMA-Y3VnP2idJRm1-KVGzg4MEoWOLxnkS9CGHVbrHKABlNA&usqp=CAU"
   },
   {
-    "id":2,
-    "photo":"abbba",
-    "title": "saiu novo filme 2",
-    "description":"ola 2"
+    "id":"2",
+    "title": "Saiu a nova versão do Logan",
+    "description":"A melhor coisa em \"Wolverine – Imortal\" é que o diretor James Mangold e os roteiristas Scott Frank e Mark Bomback conseguiram dotar o personagem Logan (Hugh Jackman) de bem mais do que as habituais garras metálicas.Mangold, Frank e Bomback foram além ao resgatar a história do personagem e dar-lhe nuances, complexidade e coração, além das habituais sequências de ação, que certamente não decepcionam. O filme estreia em versões convencionais e 3D, dubladas e legendadas.",
+    "photoCover":"https://ovicio.com.br/wp-content/uploads/2020/05/20200521-logan-movie-555x555.png"
+  },
+  {
+    "id":"3",
+    "title": "Wolverine (nascido James Howlett)",
+    "description":"Wolverine é um personagem fictício que aparece em quadrinhos americanos publicados pela Marvel Comics, principalmente em associação com os X-Men.",
+    "photoCover":"https://tm.ibxk.com.br/2023/08/21/21235126940010.jpg?ims=352x208"
+  },
+  {
+    "id":"4",
+    "title": "Wolverine no cinema: do pior ao melhor",
+    "description":"Jackman já havia afirmado que sairia da aposentadoria na pele de Logan mais uma vez caso fosse para aparecer ao lado dos Vingadores no MCU. Bem, ainda é cedo para dizer se Deadpool 3 contará com algum dos Vingadores em seu elenco, mas estamos felizes que Jackman reconsiderou tirar as garras mais uma vez a pedido do amigo. Entrando nesse clima desde agora e aquecendo os motores, resolvemos criar essa nova matéria ranqueando da pior à melhor todas as aparições de Hugh Jackman na pele do mutante mais casca grossa do cinema. Aqui, no entanto, não iremos avaliar os filmes em si, apenas as atuações do ator no personagem. Tenham isso em mente. ",
+    "photoCover":"https://viuu.com.br/wp-content/uploads/2023/07/Wolverine-no-cinema-do-pior-ao-melhor.jpg"
   }
 ];
 
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataFake}  from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -9,13 +11,31 @@ export class ContentComponent implements OnInit {
 
 
   //@Input()
-  photoCover:String="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-XEzCus7qBZWaiCmf-FtfTKJ2XbQQzjP_gnVgv1Bal8BbRBkBulnxOGvxacDtDKJmml8&usqp=CAU"
-  contentTitle:String="algo"
-  contentDescription:String="alguma coisa"
+  photoCover:String=""
+  contentTitle:String=""
+  contentDescription:String=""
+  private id:string | null="0"
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.id=value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string|null){
+    const result = dataFake.filter(article => article.id==id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+
+
   }
 
 }
